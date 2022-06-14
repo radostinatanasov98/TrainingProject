1af61743a418e6c69a16ed55285e248d4c97cc2c
Radostin Atanasov <86137037+radostinatanasov98@users.noreply.github.com>
2022-06-14 16:49:10 +0300

    Resolved namings (#2)

---
+++ API/db/migrate/20220614094324_changed_default_value_for_role_id_column_in_users_table.rb
@@ -0,0 +1,11 @@
+class ChangedDefaultValueForRoleIdColumnInUsersTable < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default(
+      :users,
+      :role_id,
+      from: 1,
+      to: 2
+    )
+  end
+
+end
