33c69ac46db107fd9a21d94cdfcfa60904429a7f
Radostin Atanasov <radostin.atanasov.1998@protonmail.com>
2022-06-14 16:42:58 +0300

    Containerization

---
+++ API/db/migrate/20220610110752_change_token_column_data_type.rb
@@ -0,0 +1,8 @@
+class ChangeTokenColumnDataType < ActiveRecord::Migration[7.0]
+  def up
+    change_column :oauth_access_tokens, :token, :string, limit: 700
+  end
+  def down
+    change_column :oauth_access_tokens, :token, :string
+  end
+end
